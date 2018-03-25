3129d7ec5769d8ac82aeb3441c2221d7fcd73125
jeyjeyemem <joeljeremy.marquez@gmail.com>
2018-03-25 11:42:05 +0800

    Initial commit

---
+++ Src/Xer.Cqrs.CommandStack.Extensions.Attributes/CommandHandlerDelegateBuilder.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xer.Delegator;
+
+namespace Xer.Cqrs.CommandStack.Extensions.Attributes
+{
+    internal class CommandHandlerDelegateBuilder
+    {
+        #region From Delegate
+        
+        internal static Func<TCommand, CancellationToken, Task> FromDelegate<TAttributed, TCommand>(Func<object> attributedObjectFactory, 
+                                                                                                    Func<TAttributed, TCommand, Task> nonCancellableAsyncDelegate)
+                                                                                                    where TAttributed : class
+                                                                                                    where TCommand : class
+        {
+            if (attributedObjectFactory == null)
+            {
+                throw new ArgumentNullException(nameof(attributedObjectFactory));
+            }
+
+            if (nonCancellableAsyncDelegate == null)
+            {
+                throw new ArgumentNullException(nameof(nonCancellableAsyncDelegate));
+            }
+
+            return (inputCommand, cancellationToken) =>
+            {
+                if (!TryGetExpectedInstanceFromFactory(attributedObjectFactory, out TAttributed instance, out Exception exception))
+                {
+                    // Exception occurred or null is returned by factory.
+                    return TaskUtility.FromException(exception);
+                }
+                
+                return nonCancellableAsyncDelegate.Invoke(instance, inputCommand);
+            };
+        }
+
+        internal static Func<TCommand, CancellationToken, Task> FromDelegate<TAttributed, TCommand>(Func<object> attributedObjectFactory, 
+                                                                                                    Func<TAttributed, TCommand, CancellationToken, Task> cancellableAsyncDelegate)
+                                                                                                    where TAttributed : class
+                                                                                                    where TCommand : class
+        {
+            if (attributedObjectFactory == null)
+            {
+                throw new ArgumentNullException(nameof(attributedObjectFactory));
+            }
+
+            if (cancellableAsyncDelegate == null)
+            {
+                throw new ArgumentNullException(nameof(cancellableAsyncDelegate));
+            }
+
+            return (inputCommand, cancellationToken) =>
+            {
+                if (!TryGetExpectedInstanceFromFactory(attributedObjectFactory, out TAttributed instance, out Exception exception))
+                {
+                    // Exception occurred or null is returned by factory.
+                    return TaskUtility.FromException(exception);
+                }
+                
+                return cancellableAsyncDelegate.Invoke(instance, inputCommand, cancellationToken);
+            };
+        }
+
+        internal static Func<TCommand, CancellationToken, Task> FromDelegate<TAttributed, TCommand>(Func<object> attributedObjectFactory, 
+                                                                                                    Action<TAttributed, TCommand> action)
+                                                                                                    where TAttributed : class
+                                                                                                    where TCommand : class
+        {
+            if (attributedObjectFactory == null)
+            {
+                throw new ArgumentNullException(nameof(attributedObjectFactory));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            return (inputCommand, cancellationToken) =>
+            {
+                try
+                {
+                    if (!TryGetExpectedInstanceFromFactory(attributedObjectFactory, out TAttributed instance, out Exception exception))
+                    {
+                        // Exception occurred or null is returned by factory.
+                        return TaskUtility.FromException(exception);
+                    }
+
+                    action.Invoke(instance, inputCommand);
+                    return TaskUtility.CompletedTask;
+                }
+                catch (Exception ex)
+                {
+                    return TaskUtility.FromException(ex);
+                }
+            };
+        }
+
+        #endregion From Delegate
+
+        #region Functions
+
+        private static bool TryGetExpectedInstanceFromFactory<TExpectedInstance>(Func<object> factory, out TExpectedInstance instance, out Exception exception) 
+            where TExpectedInstance : class
+        {            
+            // Defaults.
+            instance = null;
+
+            if(TryGetInstanceFromFactory(factory, out var factoryInstance, out exception))
+            {
+                instance = factoryInstance as TExpectedInstance;
+                if (instance == null)
+                {
+                    exception = InvalidInstanceFromFactoryDelegateException(typeof(TExpectedInstance), factoryInstance.GetType());
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool TryGetInstanceFromFactory<T>(Func<T> factory, out T instance, out Exception exception) 
+            where T : class
+        {
+            if (factory == null)
+            {
+                throw new ArgumentNullException(nameof(factory));
+            }
+            
+            // Defaults.
+            instance = null;
+            exception = null;
+
+            try
+            {
+                instance = factory.Invoke();
+                if (instance == null)
+                {
+                    // Factory returned null, no exception actually occurred.
+                    exception = FailedToRetrieveInstanceFromFactoryDelegateException<T>();
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // Wrap inner exception.
+                exception = FailedToRetrieveInstanceFromFactoryDelegateException<T>(ex);
+                return false;
+            }
+        }
+        
+        private static InvalidOperationException FailedToRetrieveInstanceFromFactoryDelegateException<TInstance>(Exception ex = null)
+        {
+            return new InvalidOperationException($"Failed to retrieve an instance of {typeof(TInstance).Name} from the instance factory delegate. Please check registration configuration.", ex);
+        }
+
+        private static InvalidOperationException InvalidInstanceFromFactoryDelegateException(Type expected, Type actual, Exception ex = null)
+        {
+            return new InvalidOperationException($"Invalid instance provided by factory delegate. Expected instnece is of {expected.Name} but was given {actual.Name}.", ex);
+        }
+
+        #endregion Functions
+    }
+}
---
+++ Tests/Xer.Cqrs.CommandStack.Extensions.Attributes.Tests/Entities/CommandHandlers.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xer.Cqrs.CommandStack;
+using Xer.Cqrs.CommandStack.Extensions.Attributes;
+using Xunit.Abstractions;
+
+namespace Xer.Cqrs.CommandStack.Extensions.Attributes.Tests.Entities
+{
+    #region Base Command Handler
+
+    public class TestCommandHandler
+    {
+        protected List<object> InternalHandledCommands { get; } = new List<object>();
+
+        protected ITestOutputHelper OutputHelper { get; }
+
+        public IReadOnlyCollection<object> HandledCommands => InternalHandledCommands.AsReadOnly();
+
+        public TestCommandHandler(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        public bool HasHandledCommand<TCommand>()
+        {
+            return InternalHandledCommands.Any(c => c is TCommand);
+        }
+
+        protected virtual void BaseHandle<TCommand>(TCommand command) where TCommand : class
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            OutputHelper.WriteLine($"{DateTime.Now}: {GetType().Name} executed command of type {command.GetType().Name}.");
+            InternalHandledCommands.Add(command);
+        }
+    }
+
+    #endregion Base Command Handler
+
+    #region Attributed Command Handlers
+
+    public class TestAttributedCommandHandler : TestCommandHandler
+    {
+
+        public TestAttributedCommandHandler(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [CommandHandler]
+        public void HandleTestCommand(TestCommand command)
+        {
+            BaseHandle(command);
+        }
+
+        [CommandHandler]
+        public void HandleThrowExceptionCommand(ThrowExceptionCommand command)
+        {
+            BaseHandle(command);
+            throw new TestCommandHandlerException("This is a triggered exception.");
+        }
+
+        [CommandHandler]
+        public Task HandleCancellableTestCommand(CancellableTestCommand command, CancellationToken ctx)
+        {
+            if(ctx == null)
+            {
+                return Task.FromException(new TestCommandHandlerException("Cancellation token is null. Please check attribute registration."));
+            }
+
+            BaseHandle(command);
+            return Task.CompletedTask;
+        }
+
+        [CommandHandler]
+        public async Task HandleDelayCommand(DelayCommand command, CancellationToken ctx)
+        {
+            await Task.Delay(command.DurationInMilliSeconds, ctx);
+
+            BaseHandle(command);
+        }
+    }
+
+    /// <summary>
+    /// This will not be allowed.
+    /// </summary>
+    public class TestAttributedSyncCommandHandlerWithCancellationToken : TestCommandHandler
+    {
+        public TestAttributedSyncCommandHandlerWithCancellationToken(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [CommandHandler]
+        public void HandleTestCommand(TestCommand command, CancellationToken cancellationToken)
+        {
+            BaseHandle(command);
+        }
+    }
+
+    #endregion Attributed Command Handlers
+    
+    public class TestCommandHandlerException : Exception
+    {
+        public TestCommandHandlerException() { }
+        public TestCommandHandlerException(string message) : base(message) { }
+    }
+}
